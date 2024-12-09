2c47083c08c9f5c64fe759cfde1c43179e5bba7a
jokimf <jokim@fode.de>
2024-12-09 12:21:10 +0100

    day7

---
+++ aoc24/7/day7-1.py
@@ -0,0 +1,21 @@
+
+from itertools import product
+lines = dict()
+with open("input.txt") as data:
+    for line in data:
+        result, values = line.split(": ")
+        lines.update({int(result): list(map(int, values.split(" ")))})
+
+
+def is_possible(result, values):
+    operator_combinations = product("+*", repeat=len(values) - 1)
+    for operators in operator_combinations:
+        intermediate = values[0]
+        for value, operator in zip(values[1:], operators):
+            intermediate = eval(f"{intermediate}{operator}{value}")
+            if result == intermediate:
+                return result
+    return 0
+
+
+print(sum([is_possible(r, v) for r, v in lines.items()]))
---
+++ aoc24/7/day7-2.py
@@ -0,0 +1,25 @@
+
+from itertools import product
+lines = dict()
+with open("input.txt") as data:
+    for line in data:
+        result, values = line.split(": ")
+        lines.update({int(result): list(map(int, values.split(" ")))})
+
+maxi = len(lines)
+count = 0
+def is_possible(result, values):
+    global count
+    count += 1
+    operator_combinations = product("+*&", repeat=len(values) - 1)
+    for operators in operator_combinations:
+        intermediate = values[0]
+        for value, operator in zip(values[1:], operators):
+            operator = "" if operator == "&" else operator
+            intermediate = eval(f"{intermediate}{operator}{value}")
+            if result == intermediate:
+                return result
+    return 0
+
+
+print(sum([is_possible(r, v) for r, v in lines.items()]))
